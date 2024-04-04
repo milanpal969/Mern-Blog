eceaa1bbfbb2098d6aa6ddea2d9bd9165ab28044
milanpal969 <palmilan568@gmail.com>
2024-04-04 15:34:52 +0530

    created routes

--- mernblog/src/App.jsx
+++ mernblog/src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/home'
+import About from './Pages/about'
+import Dashboard from './Pages/dashboard'
+import Signin from './Pages/signin'
+import Signup from './Pages/signup'
+import Projects from './Pages/projects'
+
 
 export default function App() {
   return (
-    <div className = 'text-3xl'>App</div>
+    <BrowserRouter>
+    
+    <Routes>
+
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/about' element = {<About/>}/>
+    <Route path='/dashboard' element = {<Dashboard/>}/>
+    <Route path='/signin' element = {<Signin/>}/>
+    <Route path='signup/' element = {<Signup/>}/>
+    <Route path='projects/' element = {<Projects/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
