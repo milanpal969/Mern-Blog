32cee3e7dde76130da68d8ebc121cd8adbbf491f
milanpal969 <palmilan568@gmail.com>
2024-04-04 23:58:46 +0530

    database is connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import exp from 'constants';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+
 const app = express();
+dotenv.config();
+
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("db is live");
+})
 
 app.listen(3000,()=>{
     console.log("server is live")
