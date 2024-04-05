285dc4e9c04f9f96b4f503ebf9e19ac203f0f66e
milanpal969 <palmilan568@gmail.com>
2024-04-05 12:22:03 +0530

    added functionality in footer page

--- mernblog/src/Pages/signup.jsx
+++ mernblog/src/Pages/signup.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom' 
-import {Label, TextInput,Button} from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom' 
+import {Label, TextInput,Button, Alert} from 'flowbite-react'
 
 
 export default function Signup() {
+
+  const [formdata , setdata] = useState({});
+  const [errormessage, seterror]  = useState(null);
+  const navigate  = useNavigate()
+
+
+  function handlechange(e){
+    setdata({...formdata, [e.target.id] :e.target.value.trim()})
+  }
+
+   async function handlesubmit(e){
+    e.preventDefault();
+    
+    if(!formdata.username || !formdata.email || !fromdata.password){
+      return seterror('please fill all fields')
+    }
+    try{
+      const res = await fetch('/api/auth/signup',{
+        method:"POST",
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formdata),
+      })
+
+      const data = res.json();
+
+      if(data.success===false){
+        return seterror(data.message);
+      }
+
+      if(res.ok){
+        navigate('/signin')
+      }
+    }catch(error){
+    seterror(error.message);
+    }
+  }
+
   return (
     <div className=' flex  mx-auto mt-20 flex-col md:flex-row max-w-3xl gap-5'>
       <div className=' flex-1'>
@@ -14,18 +51,20 @@ export default function Signup() {
         </div>
 
         <div className='flex-1'>
-        <form>
+
+
+        <form onSubmit={handlesubmit}>
           <div >
             <Label value= 'your username'/>
-            <TextInput type = 'text' placeholder = 'Username' id='username'/>
+            <TextInput type = 'text' placeholder = 'Username' id='username' onChange={handlechange}/>
           </div>
           <div >
             <Label value= 'your Email'/>
-            <TextInput type = 'text' placeholder = 'email' id='email'/>
+            <TextInput type = 'email' placeholder = 'email' id='email' onChange={handlechange}/>
           </div>
           <div >
             <Label value= 'your Password'/>
-            <TextInput type = 'text' placeholder = 'Password' id='pass'/>
+            <TextInput type = 'password' placeholder = 'Password' id='pass' onChange={handlechange}/>
           </div>
           <Button className='mt-2 mx-auto w-[100%]' gradientDuoTone= 'purpleToPink' type='submit'>Sing up</Button>
         </form>
@@ -34,6 +73,14 @@ export default function Signup() {
           <Link to='/signup' className='text-blue-500'>Sign up</Link>
         </div>
         </div>
+
+        {
+          errormessage && (
+            <Alert className = 'mt-5' color = 'failure'>
+            {errormessage}
+            </Alert>
+          )
+        }
     </div>
   )
 }
